87f0eb904de3625891cf4aa7bd8ab4178af148a5
durswd <swda.durl@gmail.com>
2021-04-12 22:23:18 +0900

    wip

---
+++ src/main.cpp
@@ -0,0 +1,76 @@
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "../thirdparty/stb/stb_image.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "../thirdparty/stb/stb_image_write.h"
+
+#include "../thirdparty/tiny-process-library/process.hpp"
+
+#include <iostream>
+#include <regex>
+#include <cstdio>
+#include <vector>
+
+int main()
+{
+	std::string filepath = "input.mp4";
+	// get info
+	int width = 0;
+	int height = 0;
+
+	{
+		std::string command = "ffmpeg -i " + filepath;
+		TinyProcessLib::Process process1(command, "",
+			[](const char* bytes, size_t n) {},
+			[&](const char* bytes, size_t n) {
+
+
+				std::regex re("( [0-9]+x[0-9]+ )");
+				std::smatch match;
+				auto s = std::string(bytes, n);
+				std::cout << s << std::endl;
+				if (std::regex_search(s, match, re))
+				{
+					std::sscanf(match[0].str().c_str(), " %dx%d ", &width, &height);
+				}
+			});
+		auto exit_status = process1.get_exit_status();
+	}
+
+	// load
+	{
+		// TODO : support padding
+		int imageSize = 14 + 40 + width * height * 3;
+		std::vector<char> data;
+		data.reserve(imageSize * 2);
+
+		int count = 0;
+		TinyProcessLib::Process process1("ffmpeg -i input.mp4 -c:v bmp -f rawvideo pipe:1", "", [&](const char* bytes, size_t n) {
+			const auto offset = data.size();
+			data.resize(offset + n);
+			memcpy(data.data() + offset, bytes, n);
+
+			if (data.size() >= imageSize)
+			{
+				int x = 0;
+				int y = 0;
+				int comp = 0;
+				auto p = stbi_load_from_memory((stbi_uc const*)data.data(), imageSize, &x, &y, &comp, 3);
+
+				stbi_image_free(p);
+
+				data.erase(data.begin(), data.begin() + imageSize);
+			}
+
+			// bmp
+			std::cout << n << std::endl;
+			count += n;
+			});
+		auto exit_status = process1.get_exit_status();
+		std::cout << "Example 1 process returned: " << exit_status << " (" << (exit_status == 0 ? "success" : "failure") << ")" << std::endl;
+		std::cout << count << std::endl;
+	}
+
+	return 0;
+}
